dddec4ffafac4f01d359890cd90546b16083315f
Kayes <kayes.shamil@gmail.com>
2022-08-12 22:21:55 +0600

    Project inial setup

---
+++ SearchAMovie/SearchAMovie/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//
+// SearchAMovie
+// Running on MacOS Version 12.4
+// Swift Version 5.0
+// Created by kayes on 8/12/22
+// Copyright © IMRUL KAYES. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
